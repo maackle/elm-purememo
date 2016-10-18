fcc0e8be2be6d9b7b5582adaddadd3817515c36d
Michael Dougherty <maackle.d@gmail.com>
2016-10-17 20:28:13 -0700

    Wrote some tests

--- src/Purememo.elm
+++ src/Purememo.elm
@@ -1,18 +1,18 @@
-module Purememo exposing (..)
+module Purememo exposing (Memo(..), purememo, purememoExplicit, apply, repeat, thread)
 
 import Dict exposing (Dict)
 
-type PM comparable a b = PM ((a, Dict comparable b) -> (b, Dict comparable b))
+type Memo comparable a b = Memo ((a, Dict comparable b) -> (b, Dict comparable b))
 
-apply : PM comparable a b -> Dict comparable b -> a -> (b, Dict comparable b)
-apply (PM f) d c =
+apply : Memo comparable a b -> Dict comparable b -> a -> (b, Dict comparable b)
+apply (Memo f) d c =
   f (c, d)
 
 
 -- function composition (WIP)
---fmap : (b -> z) -> PM comparable a b -> PM comparable b z
+--fmap : (b -> z) -> Memo comparable a b -> Memo comparable b z
 --fmap f p =
---  PM <| \(a, da) ->
+--  Memo <| \(a, da) ->
 --    let
 --      (b, db) = apply p da a
 --      z = f b
@@ -23,8 +23,8 @@ apply (PM f) d c =
 -- apply function to each member of the list, threading the memodict through,
 -- reusing previous calculation
 --    ex:  thread memofac Dict.empty [1..4] |> fst == [1, 2, 6, 24]
-thread : PM comparable a b -> Dict comparable b -> List a -> (List b, Dict comparable b)
-thread (PM f) d0 seq =
+thread : Memo comparable a b -> Dict comparable b -> List a -> (List b, Dict comparable b)
+thread (Memo f) d0 seq =
   let
     -- the compiler can't handle this type annotation
     -- rf : comparable -> (List b, Dict comparable b) -> (List b, Dict comparable b)
@@ -41,8 +41,8 @@ thread (PM f) d0 seq =
 
 -- repeat an operation n times, using the last result as the next input
 --   ex: repeat (purememo identity ((*) 2)) Dict.empty 2 3 |> fst == 12
-repeat : PM comparable a a -> Dict comparable a -> Int -> a -> (a, Dict comparable a)
-repeat (PM f) d0 n a0 =
+repeat : Memo comparable a a -> Dict comparable a -> Int -> a -> (a, Dict comparable a)
+repeat (Memo f) d0 n a0 =
   let
     rf _ (a0, d0) =
       let
@@ -54,7 +54,7 @@ repeat (PM f) d0 n a0 =
     (a1, d1)
 
 
-purememo : (a -> comparable) -> (a -> b) -> PM comparable a b
+purememo : (a -> comparable) -> (a -> b) -> Memo comparable a b
 purememo keyfunc f =
   let
     memoized (a, d0) =
@@ -67,9 +67,9 @@ purememo keyfunc f =
             d1 = Dict.insert k b d0
           in (b, d1)
   in
-    PM memoized
+    Memo memoized
 
-purememoExplicit : (a -> comparable) -> (a -> Dict comparable b -> b) -> PM comparable a b
+purememoExplicit : (a -> comparable) -> (a -> Dict comparable b -> b) -> Memo comparable a b
 purememoExplicit keyfunc f =
   let
     memoized (a, d0) =
@@ -82,14 +82,4 @@ purememoExplicit keyfunc f =
             d1 = Dict.insert k v d0
           in (v, d1)
   in
-    PM memoized
-
-memofac : PM Int Int Int
-memofac =
-  let
-    fac n d =
-      if n == 0 then 0
-      else if n == 1 then 1
-      else n * (fst <| apply memofac d (n - 1))
-  in
-    purememoExplicit identity fac
+    Memo memoized
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,7 +2,7 @@ module Tests exposing (..)
 
 import Dict exposing (Dict)
 
-import Purememo exposing (purememo, purememoExplicit)
+import Purememo exposing (Memo, purememo, purememoExplicit)
 import Test exposing (..)
 import Expect
 import String
@@ -12,24 +12,46 @@ all : Test
 all =
     describe "Purememo"
         [ describe "purememo"
-            [
-            --test "Factorials" <|
-            --    \() ->
-            --        Expect.equal
-            --            (snd <| memofac (4, Dict.empty))
-            --            (Dict.fromList
-            --                [ (1, 2)
-            --                ]
-            --            )
+            [ test "repeat" <|
+                \() ->
+                    let
+                        doubler = (purememo identity ((*) 2))
+                        actual = Purememo.repeat doubler Dict.empty 4 1
+                        expected =
+                            ( 16
+                            , Dict.fromList
+                                [ (1, 2)
+                                , (2, 4)
+                                , (4, 8)
+                                , (8, 16)
+                                ]
+                            )
+                    in
+                        Expect.equal actual expected
+            , test "thread" <|
+                \() ->
+                    let
+                        getLength = (purememo identity String.length)
+                        actual = Purememo.thread getLength Dict.empty ["one", "two", "three"]
+                        expected =
+                            ( [3, 3, 5]
+                            , Dict.fromList
+                                [ ("one", 3)
+                                , ("two", 3)
+                                , ("three", 5)
+                                ]
+                            )
+                    in
+                        Expect.equal actual expected
             ]
         , describe "purememoExplicit"
-            [ test "Fibonacci explicit" <|
+            [ test "Fibonacci" <|
                 \() ->
                     Expect.equal (fib 50) 12586269025
             , test "Fibonacci internals" <|
                 \() ->
                     Expect.equal
-                        (List.foldl (\n d -> snd <| memofib (n, d)) Dict.empty [0..5])
+                        (List.foldl (\n d -> snd <| Purememo.apply memofib d n) Dict.empty [0..5])
                         (Dict.fromList
                             [ (0, 0)
                             , (1, 1)
@@ -39,8 +61,25 @@ all =
                             , (5, 5)
                             ]
                         )
+            , test "Factorials" <|
+                \() ->
+                    Expect.equal
+                        (Purememo.apply memofac Dict.empty 4)
+                        (24, Dict.fromList [(4, 24)])
             ]
         ]
+
+
+memofac : Memo Int Int Int
+memofac =
+  let
+    fac n d =
+      if n == 0 then 0
+      else if n == 1 then 1
+      else n * (fst <| Purememo.apply memofac d (n - 1))
+  in
+    purememoExplicit identity fac
+
 
 memofib =
     let
@@ -49,15 +88,15 @@ memofib =
           else if n == 1 then 1
           else
             let
-              x = fst <| memofib (n - 1, d)
-              y = fst <| memofib (n - 2, d)
+              x = fst <| Purememo.apply memofib d (n - 1)
+              y = fst <| Purememo.apply memofib d (n - 2)
             in
               x + y
-    in purememoExplicit fibX
+    in purememoExplicit identity fibX
 
 fib n =
   let
-    iterations = List.foldl (\n d -> snd <| memofib (n, d)) Dict.empty [2..n]
+    iterations = List.foldl (\n d -> snd <| Purememo.apply memofib d n) Dict.empty [2..n]
   in
     case Dict.get n iterations of
       Just val -> val
